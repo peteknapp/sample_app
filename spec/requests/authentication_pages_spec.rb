990003e6a949e998abd1ac5b79a8d2622a3e3195
Pete Knapp <pete.knapp@blueprairiestudios.com>
2014-01-27 00:03:24 -0600

    Add user microposts

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -51,6 +51,19 @@ describe "Authentication" do
       it { should_not have_link('Profile',     href: user_path(user)) }
       it { should_not have_link('Settings',    href: edit_user_path(user)) }
       
+      describe "in the Microposts controller" do
+        
+        describe "submitting to the create action" do
+          before { post microposts_path }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+        
+        describe "submitting to the destroy action" do
+          before { delete micropost_path(FactoryGirl.create(:micropost)) }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+      end
+      
       describe "in the Users controller" do
       
         describe "visiting the edit page" do
